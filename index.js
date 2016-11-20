609c1e1e988e87f5c7d2fd02fe24a734f48b6c5a
Daniel Tonon <Dan503@users.noreply.github.com>
2016-11-20 11:31:02 +1100

    converted to ES5 syntax using babel-it
    I'm just going to leave it in es5 syntax since it doesn't require importing anything and only has the one export.

--- index.js
+++ index.js
@@ -1,11 +1,20 @@
+'use strict';
+
+Object.defineProperty(exports, "__esModule", {
+	value: true
+});
+
+var _typeof = typeof Symbol === "function" && typeof Symbol.iterator === "symbol" ? function (obj) { return typeof obj; } : function (obj) { return obj && typeof Symbol === "function" && obj.constructor === Symbol && obj !== Symbol.prototype ? "symbol" : typeof obj; };
+
+exports.default = replaceValues;
 
 //A function for surgically replacing values in an object
-export default function replaceValues (obj, replacements){
+function replaceValues(obj, replacements) {
 	obj = typeof obj !== 'undefined' ? obj : {};
 
 	for (var property in replacements) {
 		if (replacements.hasOwnProperty(property)) {
-			if (typeof replacements[property] === 'object' && replacements[property].constructor !== Array){
+			if (_typeof(replacements[property]) === 'object' && replacements[property].constructor !== Array) {
 				obj[property] = replaceValues(obj[property], replacements[property]);
 			} else {
 				obj[property] = replacements[property];
